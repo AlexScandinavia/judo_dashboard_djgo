bc326d638dcdf2b0ab4f56b2776ab66b51823f4d
AMATH <amath@orsted.dk>
2020-03-07 02:02:22 +0100

    Setting up the admin access for judo Results

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from app.models import JudoResult
+
+admin.site.register(JudoResult)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class JudoResult(models.Model):
+    """ Model that defines result"""
+    result = models.IntegerField()
+    event_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=20)
+    date = models.DateTimeField(verbose_name="Competition date")
+
+    class Meta:
+        verbose_name = "event result"
+        ordering = ['date']
+
+    def __str__(self):
+        return self.event_name
