55dde89e87623d12ac97965d0ef25b5f446b3b7f
AMATH <amath@orsted.dk>
2020-03-07 14:15:31 +0100

    Add Judoka model + link to JudoResult

--- app/models.py
+++ app/models.py
@@ -2,13 +2,27 @@ from django.db import models
 
 # Create your models here.
 
+class Judoka(models.Model):
+    """ Model that defines result"""
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    birthday = models.DateField(verbose_name="Birthday")
+    judoins_id = models.IntegerField()
+
+    class Meta:
+        ordering = ['judoins_id']
+
+    def __str__(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
 
 class JudoResult(models.Model):
     """ Model that defines result"""
     result = models.IntegerField()
     event_name = models.CharField(max_length=100)
     category = models.CharField(max_length=20)
-    date = models.DateTimeField(verbose_name="Competition date")
+    date = models.DateField(verbose_name="Competition date")
+    judoka = models.ForeignKey('Judoka', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "event result"
@@ -16,3 +30,4 @@ class JudoResult(models.Model):
 
     def __str__(self):
         return self.event_name
+
